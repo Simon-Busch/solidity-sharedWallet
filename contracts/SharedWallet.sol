b025e8203a1d81575aceb5d912d02c0b4720fa54
Simon Busch <simon.busch@live.fr>
2021-12-04 16:45:46 +0100

    add events

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -1,8 +1,11 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
+    //indexed let us search easily for the addresses in the side-chain
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWho, uint _oldAmount, uint uint_newAmount);
+
     mapping (address => uint) public allowance;
 
     function isOwner() internal view returns(bool) {
@@ -10,6 +13,7 @@ contract Allowance is Ownable {
     }
 
     function setAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -19,6 +23,7 @@ contract Allowance is Ownable {
     }
 
     function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount) {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
 }
